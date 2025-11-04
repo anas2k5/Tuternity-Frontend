d3af7ed6296257392ea93fd99ba0c0e66ea3a566
anas2k5 <anassyed236@gmail.com>
2025-11-04 17:29:30 +0530

    fixed major issues

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
+// api.js
 import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:8081/api",
-  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token"); // adjust if stored elsewhere
+  const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
